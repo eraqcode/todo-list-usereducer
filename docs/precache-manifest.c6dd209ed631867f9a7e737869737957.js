49d19a1fffad3f94402f1f97468c79423e039266
eraqcode <erikqtrs@gmail.com>
2020-10-07 20:57:42 -0500

    Se creo el build de produccion

---
+++ docs/precache-manifest.c6dd209ed631867f9a7e737869737957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743801b0a5b3adf7fc7071df1fd2c853",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5de6999408e96f25220",
+    "url": "/static/css/main.2f7ab010.chunk.css"
+  },
+  {
+    "revision": "2509bb302f29669887bc",
+    "url": "/static/js/2.df12b6bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.df12b6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5de6999408e96f25220",
+    "url": "/static/js/main.b2df2859.chunk.js"
+  },
+  {
+    "revision": "3221c1e833ecef9c599a",
+    "url": "/static/js/runtime-main.786d0efb.js"
+  }
+]);
